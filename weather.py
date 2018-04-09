d047474f8425094f74ff0137e1b90327a0967be4
Alejandro <antrek135@gmail.com>
2018-04-08 21:47:57 -0500

    pushing example img

---
+++ weather.py
@@ -0,0 +1,24 @@
+import sys
+import Adafruit_DHT
+from flask import Flask
+from flask import render_template
+from flask import redirect
+#from django.shortcuts import render_to_response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    data = Adafruit_DHT.read_retry(11, 21)
+    f = (data[1] * (9/5)) + 32
+    return render_template('index.html', variable = data, faren = f)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    data = Adafruit_DHT.read_retry(11, 21)
+    f = (data[1] * (9/5)) + 32
+    return render_template('index.html', variable = data, faren = f), 404
+
+if __name__ == "__main__":
+    app.run(port=5010)
